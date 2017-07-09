6c2c86370c80b31f8300e97d9d8ac4cae78dfc79
zhaozhiheng <sdut_2001@163.com>
2017-07-09 21:30:13 +0800

    增加下载成功后的处理

--- wepaysp-parent/wepaysp-api/src/main/java/com/zbsp/wepaysp/api/listener/DefaultDownloadBillBusinessResultListener.java
+++ wepaysp-parent/wepaysp-api/src/main/java/com/zbsp/wepaysp/api/listener/DefaultDownloadBillBusinessResultListener.java
@@ -1,7 +1,19 @@
 package com.zbsp.wepaysp.api.listener;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.tencent.business.DownloadBillBusiness;
 import com.tencent.protocol.downloadbill_protocol.DownloadBillResData;
+import com.zbsp.wepaysp.api.service.pay.WeixinBillService;
+import com.zbsp.wepaysp.common.util.GZipUtils;
+import com.zbsp.wepaysp.common.util.StringHelper;
 
 /**
  * User: rizenguo
@@ -9,11 +21,19 @@ import com.tencent.protocol.downloadbill_protocol.DownloadBillResData;
  * Time: 10:42
  */
 public class DefaultDownloadBillBusinessResultListener implements DownloadBillBusiness.ResultListener {
-
+	
+	protected Logger logger = LogManager.getLogger(getClass());
+	
     public static final String ON_FAIL_BY_RETURN_CODE_ERROR = "on_fail_by_return_code_error";
     public static final String ON_FAIL_BY_RETURN_CODE_FAIL = "on_fail_by_return_code_fail";
     public static final String ON_DOWNLOAD_BILL_FAIL = "on_download_bill_fail";
     public static final String ON_DOWNLOAD_BILL_SUCCESS = "on_download_bill_success";
+    
+    private WeixinBillService weixinBillService = null;
+    
+    public DefaultDownloadBillBusinessResultListener(WeixinBillService weixinBillService) {
+        this.weixinBillService = weixinBillService;
+    }
 
     private String result = "";
 
@@ -32,10 +52,17 @@ public class DefaultDownloadBillBusinessResultListener implements DownloadBillBu
         result = ON_DOWNLOAD_BILL_FAIL;
     }
 
-    @Override
-    public void onDownloadBillSuccess(String response) {
-        result = ON_DOWNLOAD_BILL_SUCCESS;
-    }
+	@Override
+	public void onDownloadBillSuccess(String response) {
+		result = ON_DOWNLOAD_BILL_SUCCESS;
+		
+		response = response.replace("`", "");
+
+		String[] bills = response.split("\\n");
+
+		weixinBillService.doTransSaveBill(bills);
+
+	}
 
     public String getResult() {
         return result;
@@ -44,5 +71,9 @@ public class DefaultDownloadBillBusinessResultListener implements DownloadBillBu
     public void setResult(String result) {
         this.result = result;
     }
+
+	public void setWeixinBillService(WeixinBillService weixinBillService) {
+		this.weixinBillService = weixinBillService;
+	}
 
 }
