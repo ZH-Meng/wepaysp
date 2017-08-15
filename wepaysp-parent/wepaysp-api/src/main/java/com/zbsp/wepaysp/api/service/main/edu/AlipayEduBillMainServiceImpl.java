24190832cd4055c42a6db34489f80272641aac76
ZH-Meng <winner90king@gmail.com>
2017-08-15 23:38:41 +0800

    临时提交;

--- wepaysp-parent/wepaysp-api/src/main/java/com/zbsp/wepaysp/api/service/main/edu/AlipayEduBillMainServiceImpl.java
+++ wepaysp-parent/wepaysp-api/src/main/java/com/zbsp/wepaysp/api/service/main/edu/AlipayEduBillMainServiceImpl.java
@@ -11,8 +11,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.alipay.api.AlipayApiException;
+import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.internal.util.codec.Base64;
 import com.alipay.api.response.AlipayEcoEduKtBillingModifyResponse;
+import com.alipay.api.response.AlipayEcoEduKtBillingQueryResponse;
 import com.zbsp.alipay.trade.config.Constants;
 import com.zbsp.wepaysp.api.service.SysConfig;
 import com.zbsp.wepaysp.api.service.edu.AlipayEduBillService;
@@ -134,16 +137,36 @@ public class AlipayEduBillMainServiceImpl
     }
     
     public static void main(String[] args) {
-        
+        String sign = "eUk+6SmV5GHjf5fz7/t0SddkuSpmGqCBLY+O4e7vQJGGyxZvzKS1F1CY8XlCmC6991zR/mpsr6TN3DViliLHC15Jvy9yaofmXD9hTsrLxrzYqPpCwY/LUhnkKZ66XztIDtO/TTVQm7ks1PLMb2UvlY69EPhTUFo7J0qKVZTmEM4=";
         try {
-            boolean b = AlipaySignature.rsaCheckContent("app_id=2016112803443590&auth_app_id=2017053107391618&buyer_id=2088202477752996&buyer_logon_id=men***@163.com&buyer_pay_amount=0.04&charset=GBK&fund_bill_list=[{\"amount\":\"0.04\",\"fundChannel\":\"ALIPAYACCOUNT\"}]&gmt_create=2017-08-15 15:14:40&gmt_payment=2017-08-15 15:14:41&invoice_amount=0.04&notify_id=db0f6f2fad963e0c9c5780d2f7141b1nn2&notify_time=2017-08-15 15:14:41&notify_type=trade_status_sync&out_trade_no=59929e85c71c884ff01e8fa1&passback_params=b3JkZXJObz01OTkyOWU4NWM3MWM4ODRmZjAxZThmYTE=&point_amount=0.00&receipt_amount=0.04&seller_email=775907359@qq.com&seller_id=2088521354078238&subject=2017���＾����5&total_amount=0.04&trade_no=2017081521001004990219233031&trade_status=TRADE_SUCCESS&version=1.0,s",
-                "Uk+6SmV5GHjf5fz7/t0SddkuSpmGqCBLY+O4e7vQJGGyxZvzKS1F1CY8XlCmC6991zR/mpsr6TN3DViliLHC15Jvy9yaofmXD9hTsrLxrzYqPpCwY/LUhnkKZ66XztIDtO/TTVQm7ks1PLMb2UvlY69EPhTUFo7J0qKVZTmEM4=", 
-                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgub5iPOR+EJfZ85xAQZvE8cRrDTNRnZNzoNK6jSpZnSql4Dl+Sb/wPQMSsOP/IvIm1qqpK1NQinLmrNKqJBT6a2rfLNqzZLLQIcMf2l/zIJWE7tR9OWEcDApe5f7UXZJx0GaK/wKJheBlo/+5bM7P+aGylE+/f9dL5FVyFeN/eX9f1yr/J6rvCNRq1vCWcZ/Sq6moGioqYckx2swHHeuarwh3QcUzKTt62zaFDlAyKJUV5co5BSMCbeJzCrFyAMr9kpuzkvlUL4cJ4l+UUiHYIkTgtZLGyOGwS9oUL7PU09sKl2dBS+a3nx+KJ05Yejl7Fn5q/wKfqBLU9YKn1comwIDAQAB", "UTF-8");
+        	System.out.println(Base64.decodeBase64(sign.getBytes()).length);
+            boolean b = AlipaySignature.rsaCheckContent("app_id=2016112803443590&auth_app_id=2017053107391618&buyer_id=2088202477752996&buyer_logon_id=men***@163.com&buyer_pay_amount=0.04&charset=GBK&fund_bill_list=[{\"amount\":\"0.04\",\"fundChannel\":\"ALIPAYACCOUNT\"}]&gmt_create=2017-08-15 15:14:40&gmt_payment=2017-08-15 15:14:41&invoice_amount=0.04&notify_id=db0f6f2fad963e0c9c5780d2f7141b1nn2&notify_time=2017-08-15 15:14:41&notify_type=trade_status_sync&out_trade_no=59929e85c71c884ff01e8fa1&passback_params=b3JkZXJObz01OTkyOWU4NWM3MWM4ODRmZjAxZThmYTE=&point_amount=0.00&receipt_amount=0.04&seller_email=775907359@qq.com&seller_id=2088521354078238&subject=2017���＾����5&total_amount=0.04&trade_no=2017081521001004990219233031&trade_status=TRADE_SUCCESS&version=1.0",
+                //"eUk+6SmV5GHjf5fz7/t0SddkuSpmGqCBLY+O4e7vQJGGyxZvzKS1F1CY8XlCmC6991zR/mpsr6TN3DViliLHC15Jvy9yaofmXD9hTsrLxrzYqPpCwY/LUhnkKZ66XztIDtO/TTVQm7ks1PLMb2UvlY69EPhTUFo7J0qKVZTmEM4=",
+                //"VXXGSaee11FbOl8V89nUCuCLz2UCxEPJyo3khbM7uL3aKk1xG9H8481LY/DgFiWjEonGOvjQ1uz5MGIIrQVG6Xkopx66PoCAj1cmZQ63mwcXl9bNb/YYRgwuaGv+wI0rgv4idw8OOsrA9XffaEQXMRtSJmC9B6Trm4r41JSlIOLIM7QVqtIYO2FZOU9dBrcX8mQXXh1O/rZwa0Y+Mm8xCBQfuZRm6GgwVZDMR650fyeU5q+aUbl0Zol55C+WVd3NR08iFCofd/nrktUo5EGgfK6tbTyRthEMcPdAKmeC/MFuY5Nh0djo1rynOvjaA/wSH9BseoeyPnUx3Qel5m7XAA==",
+            		sign,
+                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuk5ohQonX2RaUKkpgECJ6/FHBs4j7mhPR1jIc0PPKy+ypNTn54bqKUs36f01iAS6elgJi15kFnmimG/I10ezoe7YIkTUdOeh1+B/IlZVX637wwr8PyUYRn9Jcbq6KGTBoK7EakDtMcTrqkSZ1NN1tLaxUDF4bMnll9273/njeXQIDAQAB", "GBK");
             System.out.println(b);
         } catch (AlipayApiException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        
+        AlipayEduBill bill = new AlipayEduBill();
+        bill.setIsvPartnerId("2088422728707658");
+        bill.setSchoolPid("2088521354078238");
+        bill.setK12OrderNo("59929e85c71c884ff01e8fa1");
+        try {
+        	AliPayEduUtil.eduClient = new DefaultAlipayClient(
+        			"https://openapi.alipay.com/gateway.do", 
+        			"2017081308171308", 
+        			"MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCQWklPkDnUyzExlt1TrQn5txha8UtJzFdmJnnje4q3mJ01Y10HqUqfFXjw3BJs2r1PKSwAVgq2JelWRVqVL6BifNjvyrvLKsN505aRJTfDVtQYKCOQ9HHVmO9CqJ6tpwHbsPElT5pWbTSmqbOaWTTSO4nas1oLL1mYoq76zduX5kE3RqgGydlf8GAjwgqBvg85OIo4hozge3XIZmrlpsD0qLqp42Kc73l/y1b3+2YCtHG2oYN2YdenLRjdePOvploDnmExrnuFSktTCjf2hS/RXybKRK/PTXTmQ6XUc6J5Y9z/fjeCAmr5qVOa6soanGb0Pr9iFJb/Fsic6Z5aDehrAgMBAAECggEAMmDQJQxYooMnb/8Ozx0c1iGoRlTfyame9u+GqnV7PAfvFarP1NSoTkzYIHctlv/gAbiEurlMk7U3cfxSWK5+cuDPdSBz2bva7LGDYrRyeaHb83JOt1AYSktNdR4UAta+N0XRYwwc+Pyz21Zub/eiD5znx4PAcRbdVf+wwacGdERI+3JVn7M0hlPIrVjMrhYKzGukawahq7U60wExUvoctad1qQHprnfbP30swm+21YmpHkyHLEqHpwyEwzr7FdNkD6MVMOW9SGF+ToW5pnCnjahjtX4g0pzXZgoogCQUtCWR8CkTP2orRbavEypZ0MMMkM/n4mkazMsK+L0yscRIAQKBgQDPi8ghZyq6igOtONWCoZNWDN7qoOuVARWMZfqJ1E6zUApPLNfKrSlF7vycEwlENDXtAlRAhYH/UpGBkcoIrMipAVDvdQ0XFB95cMU/zEDo0B6XA//IoVVjMAWdKPd+lDqaTqwlpTUyJjhBpgWssajA2q8/hki6RJ/PKS/lTpoV6wKBgQCyDa85gVjfVVbRK459O48AEBKEkMQa23h2ltkSW83k0ImFAK0vtKjQAKP4l8zeUqcBnZWjufdZs2LHYeR7nLdBw6IB2sXyHkPoACGZNEUEItS87WDb45X/l5oCdzsUIh0asOBbuqAnAikdTsdruWU3vbrrX1J5iiLlteUct2JXgQKBgB+xoOY48vV5jgYt360LL4y8em4qFM+0Uq4WBKR3n20SlABzQKDawKrFTB4pagjYUEPZTzRrLubmnQHEWa3pdNsaMFng7hbQI95Psk+DyYXEAmfdXHou6PM0qibN0r2ptSlLSk6VZAI8g+rCh6tPlg2dV5XD0+deA2mHSKEj17PxAoGALUEfJR2EChd1nmpUp+3IVbrACiOFRFI91t5WQ71DooGCQmS+n95p/Zv8EQX8ExGs0tNLojZ08L6QP57Y5LYbOcLQXWpOX1Bj8AaAg6DuXbPAoFgxAxJgIHtWsIO/Z0mYXh1QmN/hLqj07DcGppGTcUxuwr4Sh0OdxQarOdY+T4ECgYB/zjvvU407pJ17Z0MmugP1QJ5zL61K3bE5n32DgUJbp68NJSlBGMO/XIQpcFtqwQM0YwMNLLHLw5p2huGE+ukONrcQAdYn7AFU/ULYGtU46e/gCSA0EF8qJ1QrkgJO8tCQ/smgxmbYVeS8bPV4JfJgPey8VHZ+9ZDQH6/msz6mLQ==", "json", "utf-8", 
+        			"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgub5iPOR+EJfZ85xAQZvE8cRrDTNRnZNzoNK6jSpZnSql4Dl+Sb/wPQMSsOP/IvIm1qqpK1NQinLmrNKqJBT6a2rfLNqzZLLQIcMf2l/zIJWE7tR9OWEcDApe5f7UXZJx0GaK/wKJheBlo/+5bM7P+aGylE+/f9dL5FVyFeN/eX9f1yr/J6rvCNRq1vCWcZ/Sq6moGioqYckx2swHHeuarwh3QcUzKTt62zaFDlAyKJUV5co5BSMCbeJzCrFyAMr9kpuzkvlUL4cJ4l+UUiHYIkTgtZLGyOGwS9oUL7PU09sKl2dBS+a3nx+KJ05Yejl7Fn5q/wKfqBLU9YKn1comwIDAQAB", 
+        			"RSA2");
+			AlipayEcoEduKtBillingQueryResponse response = AliPayEduUtil.billQuery(bill);
+			System.out.println(JSONUtil.toJSONString(response, true));
+		} catch (AlipayApiException e) {
+			e.printStackTrace();
+		}
     }
     
 }
